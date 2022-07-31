b5e5cc2b68f783fdd905afe7350d3f7921480f08
Brian Chou <brianchou_5@hotmail.com>
2022-07-31 17:05:15 +0800

    fixed show zero issue for sorting.py

--- Sorting.py
+++ Sorting.py
@@ -1,18 +1,18 @@
 l = []
 ll = []
 
-maxScore = int(input("Please enter the max score : \t"))
+maxScore = int(input("Please enter the max score :\t"))
 
-for i in range(maxScore+11):
+for i in range(maxScore+1):
     l.append(0)
 
-totalNumber = int(input("Please enter the total numbers : \t"))
+totalNumber = int(input("Please enter the total numbers :\t"))
 
 for i in range(0, totalNumber):
-    inputNumber = int(input("Please enter the number : \t "))
+    inputNumber = int(input("Please enter the number :\t"))
     l[inputNumber] += 1
 
-for i in range(maxScore, 0, -1):
+for i in range(maxScore, -1, -1):
     for j in range(0, l[i]):
         ll.append(i)
 
