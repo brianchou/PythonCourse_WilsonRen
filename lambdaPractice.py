dbea3eafda683a92085726410cee6172d2f45a17
Brian Chou <brianchou_5@hotmail.com>
2022-08-17 07:45:45 +0800

    Revised lambdaPractice.py

--- lambdaPractice.py
+++ lambdaPractice.py
@@ -1,19 +1,23 @@
-list  = [1, 2, 3, 4]
-list1 = []
-list2 = []
+func = [lambda x: x*x, lambda x: x+3, lambda x: x+5, lambda x: x+10, lambda x: x+20]
+listX = [1, 2, 3, 4]
 
-func = [lambda x: x*x, lambda x: x+3]
 funcCount = len(func)
-listCount = len(list)
+listCount = len(listX)
 
-for i in range(funcCount):
+listY = []
+
+listY.append(list(map(func[0], listX)))
+
+for i in range(1, funcCount):
     for j in range(listCount):
-        a = func[i](list[j])
-        list1.append(a)
-        b = func[i](list1[j])
-        list2.append(b)
+        listY.append(list(map(func[i], listY[j])))
+        if i < (funcCount-1):
+            i += 1
+print(listY[funcCount-1])
 
-print(list2[-listCount:])
+
+
+
 
 
 
